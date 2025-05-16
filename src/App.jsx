0fee8fd817c55382326bd407f66f954aee0243bf
Federico <brunetti.federico.13@gmail.com>
2025-05-16 13:12:05 +0200

    bootstrap icons importate

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Container } from "react-bootstrap";
 import MyNav from "./components/MyNav";
 import "./css/index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
   return (
--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -11,12 +11,22 @@ const MyNav = function () {
         <img src={logo} style={{ width: "100px", height: "55px" }} />
       </Navbar.Brand>
       <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">TV Shows</Nav.Link>
-        <Nav.Link href="#pricing">Movies</Nav.Link>
-        <Nav.Link href="#pricing">Recently Added</Nav.Link>
-        <Nav.Link href="#pricing">My List</Nav.Link>
+        <Nav.Link href="#prices" className="nav-link active fw-bold">
+          Home
+        </Nav.Link>
+        <Nav.Link href="#">TV Shows</Nav.Link>
+        <Nav.Link href="#">Movies</Nav.Link>
+        <Nav.Link href="#">Recently Added</Nav.Link>
+        <Nav.Link href="#">My List</Nav.Link>
       </Nav>
+      <div className="d-flex align-items-center">
+        <i className="bi bi-search icons"></i>
+        <div id="kids" className="fw-bold">
+          KIDS
+        </div>
+        <i className="bi bi-bell icons"></i>
+        <i className="bi bi-person-circle icons"></i>
+      </div>
     </Navbar>
   );
 };
